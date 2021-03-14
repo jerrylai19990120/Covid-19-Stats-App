93b38f7c8f640c15bb6355f693bee0386a1914da
jerry <jerrylai19990120@gmail.com>
2021-03-14 11:11:30 -0400

    detail view added

---
+++ Covid-19 Stats/Components/TopCountriesList.swift
@@ -0,0 +1,88 @@
+//
+//  TopCountriesList.swift
+//  Covid-19 Stats
+//
+//  Created by Jerry Lai on 2021-03-14.
+//  Copyright © 2021 Jerry Lai. All rights reserved.
+//
+
+import SwiftUI
+import SwiftUICharts
+
+struct TopCountriesList: View {
+    
+    var gr: GeometryProxy
+    
+    let styles = [
+        ChartStyle(backgroundColor:Color(red: 52/255, green: 138/255, blue: 123/255), accentColor: .white, secondGradientColor: .white, textColor: .white, legendTextColor: .white, dropShadowColor: .clear),
+        ChartStyle(backgroundColor:Color(red: 255/255, green: 131/255, blue: 104/255), accentColor: .white, secondGradientColor: .white, textColor: .white, legendTextColor: .white, dropShadowColor: .clear),
+        ChartStyle(backgroundColor: Color(red: 255/255, green: 211/255, blue: 98/255), accentColor: .white, secondGradientColor: .white, textColor: .white, legendTextColor: .white, dropShadowColor: .clear)
+    ]
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            SubHeader(gr: gr)
+            
+            ScrollView(.horizontal, showsIndicators: false) {
+                HStack {
+                    LineChartView(data: [1,2,5,32,33,37,38,39,43], title: "\(self.countryFlag(countryCode: "US")) USA", legend: "100,000,200", style: self.styles[Int.random(in: 0...2)], form: ChartForm.small).frame(width: gr.size.width*0.4, height: gr.size.height*0.23)
+                        .scaleEffect(gr.size.width*0.0018)
+                    
+                    
+                    LineChartView(data: [1,2,5,32,33,37,38,39,43], title: "\(self.countryFlag(countryCode: "US")) USA", legend: "100,000,200", style: self.styles[Int.random(in: 0...2)], form: ChartForm.small)
+                        .frame(width: gr.size.width*0.4, height: gr.size.height*0.23)
+                        .scaleEffect(gr.size.width*0.0018)
+                    
+                    LineChartView(data: [1,2,5,32,33,37,38,39,43], title: "\(self.countryFlag(countryCode: "US")) USA", legend: "100,000,200", style: self.styles[Int.random(in: 0...2)], form: ChartForm.small)
+                        .frame(width: gr.size.width*0.4, height: gr.size.height*0.23)
+                        .scaleEffect(gr.size.width*0.0018)
+                    
+                    
+                }
+                
+            }
+        }
+    }
+    
+    func countryFlag(countryCode: String) -> String {
+      let base = 127397
+      var tempScalarView = String.UnicodeScalarView()
+      for i in countryCode.utf16 {
+        if let scalar = UnicodeScalar(base + Int(i)) {
+          tempScalarView.append(scalar)
+        }
+      }
+      return String(tempScalarView)
+    }
+    
+}
+
+
+struct TopCountriesList_Previews: PreviewProvider {
+    static var previews: some View {
+        GeometryReader { gr in
+            TopCountriesList(gr: gr)
+        }
+    }
+}
+
+
+struct SubHeader: View {
+    
+    var gr: GeometryProxy
+    
+    var bgColor = Color(red: 235/255, green: 243/255, blue: 242/255)
+    var fontColor = Color(red: 52/255, green: 138/255, blue: 123/255)
+    
+    var body: some View {
+        HStack {
+            Text("Top Countries")
+                .font(.system(size: gr.size.width*0.06, weight: .bold, design: .rounded))
+                .foregroundColor(self.fontColor)
+            Spacer()
+            Text("View All")
+                .font(.system(size: gr.size.width*0.05, weight: .semibold, design: .rounded))
+                .foregroundColor(self.fontColor)
+        }.padding()
+    }
+}
--- Covid-19 Stats/Views/DetailView.swift
+++ Covid-19 Stats/Views/DetailView.swift
@@ -9,13 +9,101 @@
 import SwiftUI
 
 struct DetailView: View {
+    
+    var gr: GeometryProxy
+    
+    var activeCases = 13230033
+    
+    var bgColor = Color(red: 235/255, green: 243/255, blue: 242/255)
+    var fontColor = Color(red: 52/255, green: 138/255, blue: 123/255)
+    var tabColor = Color(red: 91/255, green: 184/255, blue: 168/255)
+    
+    @State var popup = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            fontColor
+            ZStack {
+                
+                DetailData(gr: gr, popup: $popup)
+                
+                VStack {
+                    
+                    VStack(alignment: .center) {
+                        HStack {
+                            Image(systemName: "chevron.left")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: gr.size.width*0.03, height: gr.size.width*0.03)
+                                .padding()
+                                .foregroundColor(tabColor)
+                                .overlay(RoundedRectangle(cornerRadius: 10).stroke(tabColor, lineWidth: 2))
+                            
+                            Spacer()
+                            
+                            Text("\(self.countryFlag(countryCode: "US")) USA")
+                                .foregroundColor(.white)
+                                .font(.system(size: gr.size.width*0.07, weight: .semibold, design: .default))
+                                .padding(.trailing, gr.size.width*0.1)
+                            
+                            Spacer()
+                            
+                            
+                        }.padding()
+                        
+                        Text("\(activeCases)")
+                            .foregroundColor(.white)
+                            .font(.system(size: gr.size.width*0.1, weight: .semibold, design: .default))
+                        
+                        
+                        
+                    }
+                    Spacer()
+                }
+            }
+            
+        }//.edgesIgnoringSafeArea([.top, .bottom])
+        .onAppear {
+                self.popup = true
+        }
+        .onDisappear {
+            self.popup = false
+        }
     }
+    
+    func countryFlag(countryCode: String) -> String {
+      let base = 127397
+      var tempScalarView = String.UnicodeScalarView()
+      for i in countryCode.utf16 {
+        if let scalar = UnicodeScalar(base + Int(i)) {
+          tempScalarView.append(scalar)
+        }
+      }
+      return String(tempScalarView)
+    }
+    
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        GeometryReader { gr in
+            DetailView(gr: gr)
+        }
+    }
+}
+
+struct DetailData: View {
+    
+    var gr: GeometryProxy
+    
+    @Binding var popup: Bool
+    
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 30)
+                .fill(Color.white)
+            
+            
+        }.offset(y: popup ? gr.size.height*0.23 : gr.size.height*1.2)
     }
 }
